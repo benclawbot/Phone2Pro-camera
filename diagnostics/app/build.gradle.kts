1a33dde7f4e77d9e7482dd73a8c2823187920982
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-08-03 18:36:00 +0200

    Version full specification diagnostics app

--- diagnostics/app/build.gradle.kts
+++ diagnostics/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phone2pro.diagnostics"
         minSdk = 30
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     buildTypes {
