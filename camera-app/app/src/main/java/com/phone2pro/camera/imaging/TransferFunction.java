a5ec5d97d5e456f119aefbde9c1145e2baf015c2
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-08-04 08:38:22 +0200

    Add transfer-function contract

---
+++ camera-app/app/src/main/java/com/phone2pro/camera/imaging/TransferFunction.java
@@ -0,0 +1,8 @@
+package com.phone2pro.camera.imaging;
+
+/** Transfer functions kept separate from color primaries and white point. */
+public enum TransferFunction {
+    LINEAR,
+    SRGB,
+    GAMMA_22
+}
