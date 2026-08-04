e887881756983521af1a7e40ba908d203fc2861a
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-08-04 08:50:04 +0200

    Add UI lifecycle state

---
+++ camera-app/app/src/main/java/com/phone2pro/camera/ui/AppLifecycleState.java
@@ -0,0 +1,8 @@
+package com.phone2pro.camera.ui;
+
+/** Application lifecycle visible to the camera UI state reducer. */
+public enum AppLifecycleState {
+    FOREGROUND,
+    BACKGROUND,
+    DESTROYED
+}
