365f98605ba8b386791e759c8a14cde8e933bc7c
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-08-04 09:02:51 +0200

    Add EXIF and XMP metadata field taxonomy

---
+++ camera-app/app/src/main/java/com/phone2pro/camera/storage/MetadataField.java
@@ -0,0 +1,17 @@
+package com.phone2pro.camera.storage;
+
+/** Metadata fields considered before final publication. */
+public enum MetadataField {
+    ORIENTATION,
+    CAPTURE_TIMESTAMP,
+    EXPOSURE_TIME,
+    SENSITIVITY_ISO,
+    FOCAL_LENGTH,
+    LENS_ROUTE,
+    CAPTURE_PROFILE,
+    SOFTWARE,
+    LOCATION,
+    DEVICE_MAKE_MODEL,
+    DIAGNOSTIC_XMP,
+    PROCESSING_XMP
+}
